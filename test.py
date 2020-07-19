d36a59ff548457cce424302660836904ee27aaf6
Anawin Athawong <best.gree@gmail.com>
2020-07-19 23:37:35 +0700

    fix fibo array 0

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-fibo = [1,1]
+fibo = [0,1]
 i = 1
 x = "null"
 while x != "exit" :
@@ -8,5 +8,5 @@ while x != "exit" :
     while i <= n :
         fibo.append(fibo[i] + fibo[i-1])
         i+=1
-    print("fib(%i) =" %(n), fibo[n-1])
+    print("fib(%i) =" %(n), fibo[n])
     x = input("To exit program type 'exit' : ")
